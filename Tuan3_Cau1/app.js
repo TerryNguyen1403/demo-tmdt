ad8c758c1df96db56648b4b81f8334f75ffb0330
TerryNguyen1403 <132977830+TerryNguyen1403@users.noreply.github.com>
2025-09-30 09:24:54 +0700

    Delete Tuan3_Cau1 directory

--- Tuan3_Cau1/app.js
+++
@@ -1,6 +0,0 @@
-// Nhập module math.js
-const math = require('./math');
-
-// Sử dụng các hàm đã export
-console.log(math.add(100, 52));      // Kết quả: 152
-console.log(math.subtract(100, 52)); // Kết quả: 48
--- Tuan3_Cau1/math.js
+++
@@ -1,9 +0,0 @@
-// Định nghĩa các hàm và xuất ra ngoài module
-// Định nghĩa các hàm và xuất ra ngoài module bằng exports
-exports.add = function(a, b) {
-    return a + b;
-};
-
-exports.subtract = function(a, b) {
-    return a - b;
-};
