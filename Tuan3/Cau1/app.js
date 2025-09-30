0dc64fb4ed712c26fd88b1975a4c99bcec01c01b
TerryNguyen1403 <giangnht1403@gmail.com>
2025-09-30 09:23:55 +0700

    Adding functions for ES6

--- Tuan3/Cau1/app.js
+++ Tuan3/Cau1/app.js
@@ -4,3 +4,13 @@ const math = require('./math');
 // Sử dụng các hàm đã export
 console.log(math.add(100, 52));      // Kết quả: 152
 console.log(math.subtract(100, 52)); // Kết quả: 48
+
+// Nếu dùng ES6
+
+import {
+    add,
+    substract
+} from './math'
+
+console.log(add(100, 52));      // Kết quả: 152
+console.log(substract(100, 52)); // Kết quả: 48
--- Tuan3/Cau1/math.js
+++ Tuan3/Cau1/math.js
@@ -7,3 +7,13 @@ exports.add = function(a, b) {
 exports.subtract = function(a, b) {
     return a - b;
 };
+
+// For ES6
+
+// export function add(a,b) {
+//     return a + b;
+// }
+
+// export function subtract(a,b) {
+//     return a - b;
+// }
